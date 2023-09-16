a7c02d6274fb4b0821772f1ee6f023541edb9194
brunopdt <bruno.pduarte0@gmail.com>
2023-09-16 00:21:22 -0300

    configuração projeto springboot

---
+++ implementacao/rentacar_springboot_lds/src/main/java/lds/rentacar_springboot_lds/RentacarSpringbootLdsApplication.java
@@ -0,0 +1,13 @@
+package lds.rentacar_springboot_lds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentacarSpringbootLdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentacarSpringbootLdsApplication.class, args);
+	}
+
+}
---
+++ implementacao/rentacar_springboot_lds/src/test/java/lds/rentacar_springboot_lds/RentacarSpringbootLdsApplicationTests.java
@@ -0,0 +1,13 @@
+package lds.rentacar_springboot_lds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentacarSpringbootLdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
